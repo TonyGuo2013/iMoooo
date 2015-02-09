65c9d428bfa53483e6fe592ccd27d9faf9014fa5
YexuanGuo <yexuan.guo@gmail.com>
2015-02-09 19:14:26 +0800

    add files

---
+++ application/controllers/AboutMe.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: YexuanGuo
+ * Date: 15-2-9
+ * Time: 下午7:08
+ */
+
+class AboutMeController extends Yaf_Controller_Abstract
+{
+
+    /**
+     * @return bool
+     * default reason page
+     * time 2015年02月09日19:10:14
+     */
+    public function indexAction()
+    {
+        return TRUE;
+    }
+}
+?>
