56d12d77ccc93b812323f03c3bc8915bc9991376
YexuanGuo <yexuan.guo@gmail.com>
2015-02-09 21:36:18 +0800

    fixed bugs

--- application/controllers/Index.php
+++ application/controllers/Index.php
@@ -4,7 +4,7 @@
  * @author yexuan.guo@gmail.com
  * @desc 默认控制器
  */
-class IndexController extends Yaf_Controller_Abstract {
+class IndexController extends BaseController {
 
 	/** 
      * 默认动作
@@ -13,21 +13,16 @@ class IndexController extends Yaf_Controller_Abstract {
      */
 	public function indexAction($name = "Stranger") {
 		//1. fetch query
-		$get = $this->getRequest()->getQuery("get", "default value");
-
-		//2. fetch model
-		$model = new SampleModel();
-
-		//3. assign
-		$this->getView()->assign("content", $model->selectSample());
-		$this->getView()->assign("name", $name);
+//		$get = $this->getRequest()->getQuery("get", "default value");
+//
+//		//2. fetch model
+//		$model = new SampleModel();
+//
+//		//3. assign
+//		$this->getView()->assign("content", $model->selectSample());
+//		$this->getView()->assign("name", $name);
 
 		//4. render by Yaf, 如果这里返回FALSE, Yaf将不会调用自动视图引擎Render模板
         return TRUE;
 	}
-
-    public function testAction()
-    {
-        print_R($_SERVER);
-    }
 }
