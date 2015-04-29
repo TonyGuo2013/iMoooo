f9b1450e4acdc9a75b558cc710a1c558fe6baaae
YexuanGuo <yexuan.guo@gmail.com>
2015-04-29 16:46:24 +0800

    Yexuanguo
    ReviewCode.

--- application/controllers/Index.php
+++ application/controllers/Index.php
@@ -1,28 +1,18 @@
 <?php
-/**
+/*
  * @name IndexController
  * @author yexuan.guo@gmail.com
  * @desc 默认控制器
+ * @lastModify 2015年04月29日16:45:16 Review Code.
  */
 class IndexController extends BaseController {
 
-	/** 
-     * 默认动作
-     * Yaf支持直接把Yaf_Request_Abstract::getParam()得到的同名参数作为Action的形参
-     * 对于如下的例子, 当访问http://yourhost/imoooo/index/index/index/name/TonyAnywhere_ 的时候, 你就会发现不同
-     */
 	public function indexAction($name = "Stranger") {
-		//1. fetch query
-//		$get = $this->getRequest()->getQuery("get", "default value");
-//
-//		//2. fetch model
-//		$model = new SampleModel();
-//
-//		//3. assign
-//		$this->getView()->assign("content", $model->selectSample());
-//		$this->getView()->assign("name", $name);
-
-		//4. render by Yaf, 如果这里返回FALSE, Yaf将不会调用自动视图引擎Render模板
         return TRUE;
 	}
+
+    public function aaAction()
+    {
+        print_R($_SERVER);
+    }
 }
